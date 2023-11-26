7185974ddc1439812cecc96ce96e4043772bfe28
beyzakdeniz <152064426+beyzakdeniz@users.noreply.github.com>
2023-11-26 18:24:19 +0300

    Add files via upload
    Öğrenci ve öğretmen girişinden sonra güncelle.

---
+++ admin_barı.php
@@ -0,0 +1,46 @@
+<div class="admin_barı"><br>
+<h3 class="text">Menü</h3><br><hr><br>
+<ul class="s">
+<?php
+	if(isset($_SESSION["AID"]))
+	{
+		echo'
+			<li class="li"><a href="admin_home.php">Okul Bilgisi</a></li>
+			<li class="li"><a href="ogrenci_ekle.html">Öğrenci Ekle</a></li>
+			<li class="li"><a href="calisan_ekle.php">Çalışan Ekle</a></li>
+			<li class="li"><a href="gider_ekle.php">Gider Ekle</a></li>
+			<li class="li"><a href="ogrencileri_goruntule.php">Öğrencileri Görüntüle</a></li>
+			<li class="li"><a href="velileri_goruntule.php">Velileri Görüntüle</a></li>
+			<li class="li"><a href="calisanlari_goruntule.php">Çalışanları Görüntüle</a></li>
+			<li class="li"><a href="ders_programi_goruntule.php">Ders programı Görüntüle</a></li>
+			<li class="li"><a href="giderleri_goruntule.php">Giderleri Görüntüle</a></li>
+			<li class="li"><a href="rapor_goruntule.php">Rapor Görüntüle</a></li>
+			<li class="li"><a href="logout.php">Logout</a></li>
+		
+		';
+	
+	
+	}
+	else{
+		echo'
+			<li class="li"><a href="teacher_home.php">Profile</a></li>
+			<li class="li"><a href="handle_class.php"> Handled Class</a></li>
+			<li class="li"><a href="add_stud.php"> Students</a></li>
+			<li class="li"><a href="view_stud_teach.php" target="_blank"> View Student</a></li>
+
+			<li class="li"><a href="tech_view_exam.php">View Exam</a></li>
+			<li class="li"><a href="add_mark.php">Add Marks</a></li>
+			<li class="li"><a href="view_mark.php">View Marks</a></li>
+			<li class="li"><a href="logout.php">Logout</a></li>
+
+		
+		';
+	}
+
+
+?>
+	
+
+</ul>
+
+</div>
