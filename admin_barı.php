20c4f12a4c92b650b25ebaec649bca04d079a73b
ayseberfinozcubuk <85034235+ayseberfinozcubuk@users.noreply.github.com>
2023-11-28 23:45:48 +0300

    syntax on veli_goruntule admin_barı

--- admin_barı.php
+++ admin_barı.php
@@ -9,7 +9,7 @@
 			<li class="li"><a href="calisan_ekle.html">Çalışan Ekle</a></li>
 			<li class="li"><a href="gider_ekle.html">Gider Ekle</a></li>
 			<li class="li"><a href="ogrenci_goruntule.html">Öğrencileri Görüntüle</a></li>
-			<li class="li"><a href="velileri_goruntule.html">Velileri Görüntüle</a></li>
+			<li class="li"><a href="veli_goruntule.html">Velileri Görüntüle</a></li>
 			<li class="li"><a href="calisan_goruntule.html">Çalışanları Görüntüle</a></li>
 			<li class="li"><a href="ders_programi_goruntule.html">Ders programı Görüntüle</a></li>
 			<li class="li"><a href="giderleri_goruntule.html">Giderleri Görüntüle</a></li>
