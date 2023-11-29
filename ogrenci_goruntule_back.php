6486bbf77eb6853928428023ad2c27445e8da2d3
TubaBalci <tubasultanbalci@etu.edu.tr>
2023-11-29 11:59:18 +0300

    guncellendi

--- ogrenci_goruntule_back.php
+++ ogrenci_goruntule_back.php
@@ -1,5 +1,4 @@
 <?php
-
     include "database.php";
 
     // Check connection
@@ -11,42 +10,44 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         $siralamaTuru = $db->real_escape_string($_POST['siralamaTuru']);
+        $filtrelemeTuru = $db->real_escape_string($_POST['filtrelemeTuru']);
+
         // Retrieve the entered employee name from the form
-        $ogrenciAdi = $_POST["ogrenci"];
+        $ogrenci = $_POST["ogrenci"];
 
-        if ($ogrenciAdi === '*') {
-            // Retrieve all employees if '*' is entered
-            if($siralamaTuru === 'id'){
-                $sql = "SELECT * FROM ogrenci ORDER BY ogrenci_id ASC";
-            }else if($siralamaTuru === 'isim'){
-                $sql = "SELECT * FROM ogrenci ORDER BY isim ASC";
-            }else if($siralamaTuru === 'soyisim'){
-                $sql = "SELECT * FROM ogrenci ORDER BY soyisim ASC";
-            }else if($siralamaTuru === 'dogumTarihi'){
-                $sql = "SELECT * FROM ogrenci ORDER BY dogum_tarihi ASC";
-            }  
-
-     } else {
-            // Retrieve employees based on the entered name
-            if($siralamaTuru === 'id'){
-                $sql = "SELECT * FROM ogrenci WHERE isim LIKE ? ORDER BY ogrenci_id ASC";
-            }else if($siralamaTuru === 'isim'){
-                $sql = "SELECT * FROM ogrenci WHERE isim LIKE ? ORDER BY isim ASC";
-            }else if($siralamaTuru === 'soyisim'){
-                $sql = "SELECT * FROM ogrenci WHERE isim LIKE ? ORDER BY soyisim ASC";
-            }else if($siralamaTuru === 'dogumTarihi'){
-                $sql = "SELECT * FROM ogrenci WHERE isim LIKE ? ORDER BY dogum_tarihi ASC";
-            }  
+        $sira;
+        if ($siralamaTuru === 'id') {
+            $sira = "ogrenci_id ASC";
+        } else if ($siralamaTuru === 'isim') {
+            $sira = "ogrenci_isim ASC";
+        } else if ($siralamaTuru === 'soyisim') {
+            $sira = "ogrenci_soyisim ASC";
+        } else if ($siralamaTuru === 'yas') {
+            $sira = "ogrenci_dogum_tarihi ASC";
         }
 
-        // Prepare the SQL statement
-        $stmt = $db->prepare($sql);
+        $filter;
+        if ($filtrelemeTuru === 'isim') {
+            $filter = "ogrenci_isim";
+        } else if ($filtrelemeTuru === 'soyisim') {
+            $filter = "ogrenci_soyisim";
+        } else if ($filtrelemeTuru === 'yas') {
+            $filter = "ogrenci_age";
+        }
 
-        if ($ogrenciAdi !== '*') {
-            // Bind parameters only if a specific name is entered
-            // For '*' case, no binding is necessary
-            $ogrenciAdiParam = "%$ogrenciAdi%";
-            $stmt->bind_param("s", $ogrenciAdiParam);
+        if ($ogrenci === '*') {
+            $sql = "SELECT * FROM view_ogrenci_info ORDER BY $sira";
+            $stmt = $db->prepare($sql);
+        } else {
+            $sql = "SELECT * FROM view_ogrenci_info where $filter like ? ORDER BY $sira";
+            $stmt = $db->prepare($sql);
+            // Check if the statement is prepared successfully
+            if ($stmt) {
+                $ogrenciParam = "%$ogrenci%";
+                $stmt->bind_param("s", $ogrenciParam);
+            } else {
+                die("Error preparing statement: " . $db->error);
+            }
         }
 
         // Execute the query
@@ -58,19 +59,19 @@
         // Check if there are rows in the result set
         if ($result->num_rows > 0) {
             // Output data of each row
-           echo "<table border='1'>";
-        echo "<tr><th>ID</th><th>İsim</th><th>Soyisim</th><th>Cinsiyet</th><th>Doğum Tarihi</th></tr>";
-        while ($row = $result->fetch_assoc()) {
-            echo "<tr>";
-            echo "<td>" . $row["ogrenci_id"] . "</td>";
-            echo "<td>" . $row["isim"] . "</td>";
-            echo "<td>" . $row["soyisim"] . "</td>";
-            echo "<td>" . $row["cinsiyet"] . "</td>";
-            echo "<td>" . $row["dogum_tarihi"] . "</td>";
-            // Add more fields as needed
-            echo "</tr>";
-        }
-        echo "</table>";
+            echo "<table border='1'>";
+            echo "<tr><th>ID</th><th>İsim</th><th>Soyisim</th><th>Cinsiyet</th><th>Yaş</th></tr>";
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>" . $row["ogrenci_id"] . "</td>";
+                echo "<td>" . $row["ogrenci_isim"] . "</td>";
+                echo "<td>" . $row["ogrenci_soyisim"] . "</td>";
+                echo "<td>" . $row["ogrenci_cinsiyet"] . "</td>";
+                echo "<td>" . $row["ogrenci_age"] . "</td>";
+                // Add more fields as needed
+                echo "</tr>";
+            }
+            echo "</table>";
         } else {
             echo "No results found";
         }
